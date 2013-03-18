1bc386a7ae54dbac551d64e95ebfd55e15968476
Benjamin Liles <benliles@arch.tamu.edu>
2013-03-18 13:00:38 -0500

    Tweaks on git configs

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default[:vagrant_developer][:gitignore_source]="base_gitignore"
-default[:vagrant_developer][:gitignore_destination]="/home/vagrant/.gitignore_global"
+default[:vagrant_developer][:gitignore_source]="gitignore"
+default[:vagrant_developer][:gitignore_destination]="/home/vagrant/.gitignore"
 default[:vagrant_developer][:screenfile_source]="screenrc"
 default[:vagrant_developer][:screenfile_destination]="/home/vagrant/.screenrc"
