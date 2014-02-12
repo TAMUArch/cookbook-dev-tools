666a494c2c9ba5cb6fbf223a54e3195223516a46
John Phillips <johnphillips@arch.tamu.edu>
2014-02-12 16:41:59 -0600

    Version metadata for 14.2.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jarosser06@arch.tamu.edu'
 license          'MIT'
 description      'Installs/Configures vagrant-cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '13.7.1'
+version          '14.2.1'
 
 depends "nodejs"
 depends "npm"
